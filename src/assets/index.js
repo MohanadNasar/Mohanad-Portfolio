e301a671e4ec09482ac0f0dd54a4cb0ae303fc94
Mohanad <mohannadnasar99@gmail.com>
2024-05-22 08:23:16 +0200

    Micro-issues

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,7 +21,7 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import guc from "./company/guc.png";
+import guc from "./company/GUC.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
